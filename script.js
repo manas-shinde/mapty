fcc8f0fa340640c62c5b226d51f2986911cda4d4
manas-shinde <manas.a.shinde11@gmail.com>
2023-04-25 11:53:12 +0530

    FEAT : obtaining the user's location information only when permission has been granted.

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// Get the User location
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      const {latitude,longitude} = position.coords;
+      console.log(latitude,longitude);
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    },
+    function () {
+      alert(`Couldn't able to get your location`);
+    }
+  );
+}
