9c338299614c1b87b8206309ea62dd5f7561398f
manas-shinde <manas.a.shinde11@gmail.com>
2023-04-26 09:53:45 +0530

    Rending workout input form on click.

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+let map, mapEvent;
 
 // Get the User location
 if (navigator.geolocation) {
@@ -22,34 +23,18 @@ if (navigator.geolocation) {
 
       let coords = [latitude, longitude];
 
-      const map = L.map('map').setView(coords, 13);
+      map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      // L.marker(coords)
-      //   .addTo(map)
-      //   .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-      //   .openPopup();
-
-      map.on('click', function (mapEvent) {
-        console.log(mapEvent);
-        let { lat, lng } = mapEvent.latlng;
-
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 250,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-            })
-          )
-          .setPopupContent(`📍 Current Location`)
-          .openPopup();
+      // Handling click on map
+      map.on('click', function (mapEv) {
+        mapEvent = mapEv;
+        form.classList.remove('hidden');
+        inputDistance.focus();
       });
     },
     function () {
@@ -57,3 +42,35 @@ if (navigator.geolocation) {
     }
   );
 }
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  // Clear the form fields
+  inputDistance.value =
+    inputDuration.value =
+    inputElevation.value =
+    inputCadence.value =
+      '';
+
+  console.log(mapEvent);
+  let { lat, lng } = mapEvent.latlng;
+
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+      })
+    )
+    .setPopupContent(`📍 Current Location`)
+    .openPopup();
+});
+
+inputType.addEventListener('change',()=>{
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
