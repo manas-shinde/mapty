5cf57b874858502e9f9d01c7d8d9f28c6708b4ab
manas-shinde <manas.a.shinde11@gmail.com>
2023-04-28 10:18:30 +0530

    Update map theme.

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ class App {
 
     this.#map = L.map('map').setView(this.#currentLocation, this.#mapZoomLevel);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.#map);
